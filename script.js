0f1705e722fded827b1ab03c24f04511d38cce42
Gajula Sathish <110405590+hackersfun369@users.noreply.github.com>
2025-09-12 01:26:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth Scroll
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
